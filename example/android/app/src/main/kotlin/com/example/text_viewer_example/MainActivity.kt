9d54fc664d73b9da975a572141d048befda1afcb
codingwithtashi <khakontas33@gmail.com>
2022-05-21 20:44:46 +0530

    flutter text viewer example added

---
+++ example/android/app/src/main/kotlin/com/example/text_viewer_example/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.text_viewer_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ example/lib/main.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_text_viewer/flutter_text_viewer.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'TextViewer Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+          child: const Text('Load asset text'),
+          onPressed: () async {
+            Navigator.push(context, MaterialPageRoute(
+              builder: (context) {
+                return TextViewerPage(
+                  textViewer: TextViewer.asset(
+                    'assets/test.txt',
+                    highLightColor: Colors.yellow,
+                    focusColor: Colors.orange,
+                    ignoreCase: true,
+                  ),
+                  showSearchAppBar: true,
+                );
+              },
+            ));
+          },
+        ),
+      ),
+    );
+  }
+}
