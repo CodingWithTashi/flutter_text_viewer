ce88eacb45418477d4843ae20bb95f98caa1f7ef
codingwithtashi <khakontas33@gmail.com>
2022-11-29 15:05:09 +0530

    removed unused code

--- test/flutter_text_viewer_test.dart
+++ test/flutter_text_viewer_test.dart
@@ -1,13 +1,5 @@
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_text_viewer/flutter_text_viewer.dart';
-
 void main() {
-  test('adds one to input values', () {
-    final calculator = TextViewerPage(
-      textViewer: TextViewer.asset(''),
-      showSearchAppBar: false,
-    );
-  });
+  test('adds one to input values', () {});
 }
